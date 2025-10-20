67e5e6efe13b80ddc2bb5ef54e2479c93300b719
Ernest Mpiani <ernestmpiani15@gmail.com>
2025-10-20 08:48:43 +0000

    No description

--- voting_software/settings.py
+++ voting_software/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$cj5pj^dt-kmpi0!qf)q-5rrpiu*0hp)04=@bc$na6-s^@@pxi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
